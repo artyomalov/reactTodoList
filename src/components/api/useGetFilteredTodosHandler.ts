6a716e7be0954369e42f024fb512790540b9cc10
artyomalov <artyomalov@gmail.com>
2023-05-04 17:39:40 +0300

    changed url

--- src/components/api/useGetFilteredTodosHandler.ts
+++ src/components/api/useGetFilteredTodosHandler.ts
@@ -1,4 +1,4 @@
-import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import { useAppDispatch } from '../../store/hooks';
 import todoRequests from './requests';
 import { getAllTodos } from '../../store/todoSlice';
 
@@ -9,15 +9,16 @@ export const useGetFilteredTodos = () => {
 
   const getFilteredTodos = async (filterValue: string, pageNumber: number) => {
     try {
+      console.log(filterValue, pageNumber)
       const response = await todoRequests.getAllTodos(filterValue, pageNumber);
       if (response.status !== 200) {
         throw new Error('Server error!');
       }
+      console.log(response)
       dispatch(getAllTodos(response.data));
     } catch (err) {
       console.log(err);
     }
   };
-
   return getFilteredTodos;
 };
